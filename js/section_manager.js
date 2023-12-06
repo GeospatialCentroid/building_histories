443cabf2778e1a7ca8a5b9e6d7c8ca9694ca007e
kevinworthington <kworthin@gmail.com>
2023-12-06 14:04:33 -0700

    added new window icon and delay for showing sections

--- js/section_manager.js
+++ js/section_manager.js
@@ -324,6 +324,8 @@ class Section_Manager {
             setTimeout(() => {
                $("#section_id_0").trigger("click");
                 $("#arrow_0").trigger("click");
+
+                setTimeout(() => {$("#panels").show();} , "500");
                if(this.json_data[0]?.legend){
 
                 layer_manager.create_legend(JSON.parse(this.json_data[0]?.legend),"section_id_1")
